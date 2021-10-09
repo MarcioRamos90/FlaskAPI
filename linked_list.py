aa30978edb42be80c39561b7079b5f5d061619ca
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-10-09 15:10:07 -0300

    create linked list feature

--- linked_list.py
+++ linked_list.py
@@ -8,3 +8,51 @@ class LinkedList:
   def __init__(self):
     self.head = None
     self.last_node = None
+
+  def get_by_id(self, id: int):
+    node = self.head
+    while node:
+      if node.data["id"] == int(id):
+        return node.data
+      node = node.next_node
+    return None
+
+  def to_list(self):
+    list = []
+    if self.head is None:
+      return list
+    
+    node = self.head
+    while node:
+      list.append(node.data)
+      node = node.next_node
+    return list
+
+
+  def print_ll(self):
+    ll_string = ""
+    node = self.head
+    if node == None:
+      print(None)
+    while node:
+      ll_string += f" {str(node.data)} ->"
+      if node.next_node is None:
+        ll_string += " None"
+      node = node.next_node
+    print(ll_string)
+
+  def insert_begining(self, data):
+    if self.head is None:
+      self.head = Node(data, None)
+      self.last_node = self.head
+    else:
+      new_node = Node(data, self.head)
+      self.head = new_node
+
+  def insert_at_end(self, data):
+    if self.head is None:
+      self.insert_begining(data)
+      return
+    self.last_node.next_node = Node(data, None)
+    self.last_node = self.last_node.next_node
+
